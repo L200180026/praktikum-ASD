4cd20ebbb0b2e72efb5f9d9750b3c3261ff8cff3
L200180026 <48095287+L200180026@users.noreply.github.com>
2020-03-20 22:04:03 +0700

    Add files via upload

---
+++ MODUL- 04/Tugas/02.py
@@ -0,0 +1,38 @@
+class MhsTIF(object):
+    def __init__(self, nama, nim, kota, uangsaku):
+        self.nama = nama
+        self.nim = nim
+        self.kotaTinggal = kota
+        self.uangSaku = uangsaku
+
+
+class buatArray(object):
+    internalData = 11 * [None]
+
+    def __getitem__(self, item):
+        return self.internalData[item]
+
+    def __setitem__(self, key, value):
+        self.internalData[key] = value
+
+    def cariUangsaku(self):
+        terkecil = self[0].uangSaku
+        for i in self:
+            if i.uangSaku < terkecil:
+                terkecil = i.uangSaku
+        return print(terkecil)
+
+
+c = buatArray()
+c[0] = MhsTIF('Novera', 10, 'Batang', 250000)
+c[1] = MhsTIF('Budi', 51, 'Solo', 200000)
+c[2] = MhsTIF('Andi', 2, 'Wonogiri', 245000)
+c[3] = MhsTIF('Susanto', 18, 'Bandung', 240000)
+c[4] = MhsTIF('Rio', 4, 'Surakarta', 235000)
+c[5] = MhsTIF('Bowo', 31, 'Sragen', 260000)
+c[6] = MhsTIF('Billy', 10, 'Klaten', 250000)
+c[7] = MhsTIF('Putri', 5, 'Semarang', 220000)
+c[8] = MhsTIF('Denis', 64, 'Klaten', 225000)
+c[9] = MhsTIF('Novi', 23, 'Kendal', 215000)
+c[10] = MhsTIF('Dina', 29, 'Purwodadi', 245000)
+
---
+++ MODUL- 04/Tugas/07.py
@@ -0,0 +1,14 @@
+def binSe(kumpulan, target):
+    low = 0
+    high = len(kumpulan) - 1
+    listku = []
+
+    while low <= high:
+        if kumpulan[low] == target:
+            listku.append(low)
+            low += 1
+        else:
+            low += 1
+    return listku
+
+
